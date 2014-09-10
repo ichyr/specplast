d292c7fccc9e3896ad90972964fb7531e404217d
ichyr <ichyr@softserveinc.com>
2014-09-10 11:39:57 +0300

    specified file extention in html fold - only pdf

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -18,7 +18,7 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "404.pdf"
+    "../404.html"
   end
 
   # Process files as they are uploaded:
@@ -35,14 +35,18 @@ class FileUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(pdf doc docx)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def extentions_for_accept_attribute
+    "application/pdf"
+  end
 
 end
