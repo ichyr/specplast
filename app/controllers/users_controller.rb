f6f0091aa6faca706ca820e00230d53e79dca61a
ichyr <ichyr@softserveinc.com>
2014-10-14 13:33:05 +0300

    added :avatar to strong params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :role, :specialization_id)
+    params.require(:user).permit(:name, :email, :role, :specialization_id, :avatar)
   end
 end
