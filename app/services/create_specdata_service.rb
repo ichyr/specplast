c32a3d276bd9c92a1358994557f80df9df07d568
ichyr <ichyr@softserveinc.com>
2014-11-03 16:35:10 +0200

    introduces HERE doc for seed of specdata in service

--- app/services/create_specdata_service.rb
+++ app/services/create_specdata_service.rb
@@ -1,8 +1,12 @@
 class CreateSpecdataService
+
+	GENERAL_INFO = <<-DOCS
+	  This is a block of General information
+	DOCS
   
   def call text, spec_id
     data = Specdatum.find_or_create_by!(specialization_id: spec_id) do |data|
-    	data.gen_info = text
+    	data.gen_info = GENERAL_INFO
       data.proby = text
       data.activity = text
       data.provid = text
