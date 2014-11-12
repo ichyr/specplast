2e3ad2ac26ba3a7ea41741d76ccfa3c4824ae663
ichyr <ichyr@softserveinc.com>
2014-11-12 18:13:24 +0200

    added dependent destroy for specdatum in specialization model:

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -1,7 +1,7 @@
 class Specialization < ActiveRecord::Base
 	mount_uploader :avatar, AvatarUploader
 	
-	has_one :specdatum
+	has_one :specdatum, dependent: :destroy
 	has_many :moderators, :foreign_key => 'specialization_id', :class_name => "User"
 	has_many :vmilists
 
