cbfd5bf6375b9681ca5df14a20fd0a3a074a6eea
ichyr <ichyr@softserveinc.com>
2014-11-12 12:12:59 +0200

    minor stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require bootsy
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
 
 $(document).ready(function () {
