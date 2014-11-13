6c9655ada0aff867ac21ac993b82302fb3afa928
ichyr <ichyr@softserveinc.com>
2014-11-13 08:38:31 +0200

    added all fields to DEVISE user edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
-  include Pundit
   protect_from_forgery with: :exception
 
+  
+  include Pundit
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :role
+    devise_parameter_sanitizer.for(:account_update) << :specialization_id
+    devise_parameter_sanitizer.for(:account_update) << :avatar
+  end
 
   private
 
