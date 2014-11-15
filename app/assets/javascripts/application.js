bf32ab0f175726a9205702441057c26e354e7629
ichyr <ichyr@softserveinc.com>
2014-11-15 22:36:58 +0200

    implemented active tab reflection in the url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,20 @@
 //= require bootsy
 //= require turbolinks
 //= require_tree .
+
+// make tabs reflect actual state in url
+var ready;
+ready = function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    var scrollmem = $('body').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
