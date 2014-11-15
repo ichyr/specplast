7413b434d002522df9eb12215e0f33885192bad2
ichyr <ichyr@softserveinc.com>
2014-11-15 22:40:07 +0200

    refactored sticky_tabs

--- app/assets/javascripts/sticky_tabs.js
+++ app/assets/javascripts/sticky_tabs.js
@@ -1,4 +1,14 @@
-$(function(){
+// make tabs reflect actual state in url
+// code taken from
+
+// push state change on tabs change
+// http://stackoverflow.com/questions/12131273/twitter-bootstrap-tabs-url-doesnt-change
+
+// turbolinks issue
+// http://stackoverflow.com/questions/18770517/rails-4-how-to-use-document-ready-with-turbo-links
+
+var ready;
+ready = function(){
   var hash = window.location.hash;
   hash && $('ul.nav a[href="' + hash + '"]').tab('show');
 
@@ -8,4 +18,7 @@ $(function(){
     window.location.hash = this.hash;
     $('html,body').scrollTop(scrollmem);
   });
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
