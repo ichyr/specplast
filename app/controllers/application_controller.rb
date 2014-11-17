2dcb1366dbebe8a0fa525057cec929237ee2c653
ichyr <ichyr@softserveinc.com>
2014-11-17 20:36:56 +0200

    localized devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # I18n.locale = :ua
+
   protected
 
   def configure_permitted_parameters
