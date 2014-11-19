827e6efee7357effeaf3152a27d008cb1ee52c80
ichyr <ichyr@softserveinc.com>
2014-11-19 19:57:49 +0200

    added config.exceptions_app = self.routes to application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  config.exceptions_app = self.routes
   
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
