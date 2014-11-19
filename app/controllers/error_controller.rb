2641bf5e80361d12821433047d92195fce6ff2fa
ichyr <ichyr@softserveinc.com>
2014-11-19 19:46:14 +0200

    created errorController for 404 and 500 errors

---
+++ app/controllers/error_controller.rb
@@ -0,0 +1,7 @@
+class ErrorController < ApplicationController
+  def error_404
+  end
+
+  def error_500
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'moderator', to: 'moderator#index'
-  get 'moderator/specialization', to: 'moderator#specialization'
-
-  get 'admin', to: 'admin#index'
-  get 'admin/instruktors', to: 'admin#instruktors'
-  get 'admin/moderators', to: 'admin#moderators'
-  get 'admin/administrators', to: 'admin#administrators'
-  get 'admin/bulava', to: 'admin#bulava'
+  root to: 'visitors#index'
 
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
-	root to: 'visitors#index'
 
-  devise_for :users
-
-  get "activities", to: 'visitors#activities'
-  get "vmilosti", to: 'visitors#vmilosti'
-  get "proby", to: 'visitors#proby'
-  get "instructors", to: 'visitors#instructors'
-  get "provid", to: 'visitors#provid'
-  get "bulava", to: 'visitors#bulava'
-  get "visitors/api/:id", to: 'visitors#api'
 
   resources :vmilists
 
@@ -30,10 +13,35 @@ Rails.application.routes.draw do
 
   resources :general_infos
 
+  get 'moderator', to: 'moderator#index'
+  get 'moderator/specialization', to: 'moderator#specialization'
+
+  get 'admin', to: 'admin#index'
+  get 'admin/instruktors', to: 'admin#instruktors'
+  get 'admin/moderators', to: 'admin#moderators'
+  get 'admin/administrators', to: 'admin#administrators'
+  get 'admin/bulava', to: 'admin#bulava'
+
+  # Visitors controller
+  get "activities", to: 'visitors#activities'
+  get "vmilosti", to: 'visitors#vmilosti'
+  get "proby", to: 'visitors#proby'
+  get "instructors", to: 'visitors#instructors'
+  get "provid", to: 'visitors#provid'
+  get "bulava", to: 'visitors#bulava'
+  get "visitors/api/:id", to: 'visitors#api'
+  
   get 'users', to: 'users#index'
+  #  User controller
+  devise_for :users
   get 'users/:id', to: 'users#show', :as => :user
   get 'users/edit/:id', to: 'users#edit'
   put 'users/:id', to: 'users#update'
   patch 'users/:id', to: 'users#update'
   delete 'users/:id', to: 'users#delete'
+
+  # Exceptions and errors 
+  get '404', to: 'error#error_404'
+  get '500', to: 'error#error_500'
+  
 end
