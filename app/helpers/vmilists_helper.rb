104656e0c553a5e941bdc9f840338cbe23c5d17a
ichyr <ichyr@softserveinc.com>
2014-12-24 19:02:25 +0200

    added decoration helper for Vmilists lists in user lists

--- app/helpers/vmilists_helper.rb
+++ app/helpers/vmilists_helper.rb
@@ -1,2 +1,10 @@
 module VmilistsHelper
+	def decorate_set vmilists
+		result = "<"
+		vmilists.each { |vmilist|
+			result += link_to vmilist.name, vmilist, class: 'btn-xs'
+			result += "\n"
+		}
+		result
+	end
 end
