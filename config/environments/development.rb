5f56a003cba3fbe6f5fa05a44b2a19bab343ec72
ichyr <ichyr@softserveinc.com>
2015-01-10 00:07:54 +0200

    gitignore secrets.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,13 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -38,12 +45,6 @@ Rails.application.configure do
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
-  # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
 
 
   # Adds additional error checking when serving assets at runtime.
