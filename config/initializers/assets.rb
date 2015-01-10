48cf8483bae44f8a8371356217e849397e681924
ichyr <ichyr@softserveinc.com>
2015-01-10 23:57:37 +0200

    added script for .env.yml parse with ENV variables

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,13 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '0.6'
 
+Rails.application.config.before_configuration do
+  env_file = File.join(Rails.root, '.env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
