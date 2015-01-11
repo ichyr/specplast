b33c5f3e132b8891914412159d88d23d06f454c2
ichyr <ichyr@softserveinc.com>
2015-01-12 00:42:42 +0200

    added devise mailer to deliver_later

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,7 +3,7 @@ class RegistrationController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UserMailer.new_registration(@user).deliver_now
+      UserMailer.new_registration(@user).deliver_later
     end
   end
 
--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,12 +1,27 @@
-class CustomDeviseMailer < Devise::Mailer
+class CustomDeviseMailer
   title = I18n.t("layout.title")
   default from: "'#{title}' #{Rails.application.secrets.email_provider_username}"
 
-  puts "DEVISE mailer"
-  puts
-  puts title
-  puts
-  puts "'#{title}' #{Rails.application.secrets.email_provider_username}"
-  puts
-  puts "DEVISE mailer"
+  def self.confirmation_instructions(record, opts = {})
+    new(:confirmation_instructions, record, opts)
+  end
+
+  def self.reset_password_instructions(record, opts = {})
+    new(:reset_password_instructions, record, opts)
+  end
+
+  def self.unlock_instructions(record, opts = {})
+    new(:unlock_instructions, record, opts)
+  end
+
+  def initialize(method, record, opts = {})
+    @method, @record, @opts = method, record, opts
+  end
+
+  def deliver
+    # You need to hardcode the class of the Devise mailer that you
+    # actually want to use. The default is Devise::Mailer.
+    Devise::Mailer.delay.send(@method, @record, @opts)
+  end
+
 end
