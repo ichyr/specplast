0de057582cf7276c095dff59a0573d942214179b
ichyr <ichyr@softserveinc.com>
2015-01-11 22:17:56 +0200

    made visitor#instructors tabs sortable :name, :city: region. with bootstrap glyph.chevron pointing sort direction

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,8 @@
 class VisitorsController < ApplicationController
 
+	# for access in views
+	helper_method :sort_column, :sort_direction
+
 	def index		
 		@specializations = Specialization.order(name: :asc)
 	end
@@ -18,6 +21,7 @@ class VisitorsController < ApplicationController
 		@instruktors = User.select(:id, :name, :avatar, :city, :region)
 											 .includes(:vmilists)
 											 .where("lower(name) like ?", "%#{params[:search].downcase if params[:search]}%")
+											 .order(sort_column + " " + sort_direction)
 											 .paginate(:page => params[:page], :per_page => 10)
 	end
 
@@ -45,5 +49,15 @@ class VisitorsController < ApplicationController
 											 	      "%#{params[:search].downcase if params[:search]}%", "#{params[:id]}")
 			.limit(10)
 	end
+
+  private
+  
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 
 end
---
+++ app/helpers/visitors_helper.rb
@@ -0,0 +1,19 @@
+module VisitorsHelper
+	def sortable column, title = nil
+		column = column.to_s
+	  title ||= column.titleize
+	  css_class = column == sort_column ? "current #{sort_direction}" : nil
+	  direction = ( column == sort_column && sort_direction == "asc" ) ? "desc" : "asc"
+
+	  title = sort_glyph(title, css_class).html_safe
+	  link_to title, { sort: column, direction: direction }, { class: css_class }
+	end
+
+	def sort_glyph title, css_class
+		if css_class 
+			title += " <span class='glyphicon glyphicon-chevron-down'></span>" if sort_direction == "desc"
+			title += " <span class='glyphicon glyphicon-chevron-up'></span>" if sort_direction == "asc"
+		end
+		title
+	end
+end
