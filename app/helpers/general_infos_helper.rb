c23f3bf9c6651ce573feff71ceb7e262969146d4
ichyr <ichyr@softserveinc.com>
2015-01-11 21:04:00 +0200

    Khrystja.11.10.7.

--- app/helpers/general_infos_helper.rb
+++ app/helpers/general_infos_helper.rb
@@ -1,2 +1,6 @@
 module GeneralInfosHelper
+	def last_updated_at specdatum
+		result = I18n.t("layout.specialization.information_last_updated")
+		result += I18n.l(specdatum.updated_at, format: "%d %B %Y ")
+	end
 end
