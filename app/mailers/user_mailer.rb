4a2da0287ac6ead227c5981e318dce42716f126f
ichyr <ichyr@softserveinc.com>
2015-01-11 08:49:24 +0200

    letters are now delivered

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
 
 	def new_registration user
 		@user = user
-    mail(:to => user.email, 
-    		 :subject => "Welcome to Specplast Technologies!!!") do |format|
+    mail(to: user.email, 
+    		 subject: "Welcome to Specplast Technologies!!!") do |format|
       format.html { render layout: 'mailer' }
       format.text
     end
