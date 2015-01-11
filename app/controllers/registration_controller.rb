e2790c2e3f0c7d3977ca01f3a3d9c3f36969441c
ichyr <ichyr@softserveinc.com>
2015-01-11 08:26:06 +0200

    changed the place for loading of enviramental variables from .env.yml

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,7 +3,7 @@ class RegistrationController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UserMailer.new_registration(@user).deliver
+      UserMailer.new_registration(@user).deliver_now
     end
   end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+
+	def new_registration user
+		@user = user
+    mail(:to => user.email, 
+    		 :subject => "Welcome to Specplast Technologies!!!") do |format|
+      format.html { render layout: 'mailer' }
+      format.text
+    end
+	end
 end
