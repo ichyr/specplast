8d16226b5976346aa18d29b48c3b1cc43121b2a3
ichyr <ichyr@softserveinc.com>
2015-01-12 12:34:12 +0200

    changed in previous files username and projectname

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,10 @@
-root = "/home/username/apps/projectname/current"
+root = "/home/specplast/apps/specplast/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.projectname.sock"
+listen "/tmp/unicorn.specplast.sock"
 worker_processes 2
 timeout 30
 
