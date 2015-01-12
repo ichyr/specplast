2f8fdee26790b9ecf0d0a251d5bf840fda21e28d
ichyr <ichyr@softserveinc.com>
2015-01-12 22:22:41 +0200

    explicitly declare origin/production

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,8 +28,8 @@ namespace :deploy do
 
   desc "Makes sure local git is in sync with remote."
   task :check_revision do
-    unless `git rev-parse HEAD` == 'git rev-parse origin/#{fetch(:branch)}'
-      puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+    unless `git rev-parse HEAD` == 'git rev-parse origin/production'
+      puts "WARNING: HEAD is not the same as origin/production"
       puts "Run `git push` to sync changes."
       exit
     end
