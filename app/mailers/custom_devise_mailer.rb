c12a9fa410315c90d9022966fa41dd59ece45f2c
ichyr <ichyr@softserveinc.com>
2015-01-14 00:30:51 +0200

    removed debug lines

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,12 +1,4 @@
 class CustomDeviseMailer < Devise::Mailer
   title = I18n.t("layout.title")
   default from: "'#{title}' #{Rails.application.secrets.email_provider_username}"
-
-  puts "DEVISE mailer"
-  puts
-  puts title
-  puts
-  puts "'#{title}' #{Rails.application.secrets.email_provider_username}"
-  puts
-  puts "DEVISE mailer"
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,10 +6,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Specplast::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+Rails.application.config.secret_token = if Rails.env.development? or Rails.env.test?
   ('x' * 30) # meets minimum requirement of 30 chars long
 else
   ENV['SPECPLAST_PRODUCTION_SECRET_TOKEN'] || SecureRandom.hex(64)
 end
 
-Specplast::Application.config.secret_key_base = Rails.application.secrets.secret_key_base
+Rails.application.config.secret_key_base = Rails.application.secrets.secret_key_base
