ac9ba40f53b072a7166fe7e48ec1d3895b8b5ebf
ichyr <ichyr@softserveinc.com>
2015-01-13 23:52:46 +0200

    cahnged secrects.yml . introduced secret_token

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,17 @@
 root = "/var/www/specplast_prod/current"
+
 working_directory root
+
 pid "#{root}/tmp/pids/unicorn.pid"
+
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
 listen "/tmp/unicorn.specplast.sock"
+listen 8080, :tcp_nopush => true
+
 worker_processes 2
 timeout 30
 
