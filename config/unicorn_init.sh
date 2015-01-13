f5c516be978ca5a730298f8e87b423ee808cef1b
ichyr <ichyr@softserveinc.com>
2015-01-13 20:45:58 +0200

    updated to real path in unicorn_init.sh

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/specplast/apps/specplast/current
+APP_ROOT=/var/www/specplast_prod/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=specplast
