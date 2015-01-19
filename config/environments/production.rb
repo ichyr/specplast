279e5112a4e3d9f4a21073b5737cf6b38d937c76
ichyr <ichyr@softserveinc.com>
2015-01-19 20:17:30 +0200

    translated registration welcome message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
 
   # Disable automatic flushing of the log to improve performance.
