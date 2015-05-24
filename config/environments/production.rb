9853ad9f568fcf5731b70355cd70e07759a7900e
ichyr <ichyr@softserveinc.com>
2015-05-24 15:08:54 +0300

    changed path to assets in nginx.conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
