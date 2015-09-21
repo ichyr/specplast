bc82b10e425776d05fb4e3f79897947246e64eb1
ichyr <zmii@plast.org.ua>
2015-09-21 20:05:37 +0300

    removed decoration of main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,11 +16,11 @@ module ApplicationHelper
 	def decorate_specialization_with_state(specialization)
 		result = "<div>"
 			result += "<img src=" + specialization.avatar.url(:thumb) + ">" 
-				if specialization.status == 0
-					result += "<span class='glyphicon glyphicon-hourglass overlay status-pending'> </span>" 
-				else
-					result += "<span class='glyphicon glyphicon-ok-sign overlay status-accepted'> </span>" 
-				end
+				# if specialization.status == 0
+				# 	result += "<span class='glyphicon glyphicon-hourglass overlay status-pending'> </span>" 
+				# else
+				# 	result += "<span class='glyphicon glyphicon-ok-sign overlay status-accepted'> </span>" 
+				# end
 			result += "</img>" 
 		result += "</div>"
 
