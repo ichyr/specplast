b2eea28d7fda5a716e80f6f25a1aea0e035b85a1
ichyr <zmii@plast.org.ua>
2015-10-18 02:07:43 +0300

    added page titles to devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
 	def decorate_vmilist_with_state(vmilist)
 		result = "<div>"
-			result += "<img src=" + vmilist.avatar.url(:thumb) + ">" 
-				if vmilist.status == 0
-					result += "<span class='glyphicon glyphicon-hourglass overlay status-pending'> </span>" 
-				else
-					result += "<span class='glyphicon glyphicon-ok-sign overlay status-accepted'> </span>" 
-				end
-			result += "</img>" 
+		result += "<img src=" + vmilist.avatar.url(:thumb) + ">"
+		if vmilist.status == 0
+			result += "<span class='glyphicon glyphicon-hourglass overlay status-pending'> </span>"
+		else
+			result += "<span class='glyphicon glyphicon-ok-sign overlay status-accepted'> </span>"
+		end
+		result += "</img>"
 		result += "</div>"
 
 		result
@@ -15,15 +15,19 @@ module ApplicationHelper
 
 	def decorate_specialization_with_state(specialization)
 		result = "<div>"
-			result += "<img src=" + specialization.avatar.url(:thumb) + ">" 
-				# if specialization.status == 0
-				# 	result += "<span class='glyphicon glyphicon-hourglass overlay status-pending'> </span>" 
-				# else
-				# 	result += "<span class='glyphicon glyphicon-ok-sign overlay status-accepted'> </span>" 
-				# end
-			result += "</img>" 
+		result += "<img src=" + specialization.avatar.url(:thumb) + ">"
+		# if specialization.status == 0
+		# 	result += "<span class='glyphicon glyphicon-hourglass overlay status-pending'> </span>"
+		# else
+		# 	result += "<span class='glyphicon glyphicon-ok-sign overlay status-accepted'> </span>"
+		# end
+		result += "</img>"
 		result += "</div>"
 
 		result
+	end
+
+	def set_page_title(page_title)
+		content_for :title, page_title.to_s
 	end
 end
