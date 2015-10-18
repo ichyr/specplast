13301bda7899482bf92aa0f942959519591072de
ichyr <zmii@plast.org.ua>
2015-10-18 03:44:01 +0300

    fixed tests

--- spec/models/general_info_spec.rb
+++ spec/models/general_info_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe GeneralInfo, :type => :model do
 
 	before :each do
-		general_info = FactoryGirl.build(:general_info)
+		@general_info = FactoryGirl.build(:general_info)
 	end
 	
   it "should have present key" do
-  	expect(general_info).to validate_presence_of(:key)
+  	expect(@general_info).to validate_presence_of(:key)
   end
 
   it "should have present value" do
-  	expect(general_info).to validate_presence_of(:value)
+  	expect(@general_info).to validate_presence_of(:value)
   end
 end
