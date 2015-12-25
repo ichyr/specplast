dc3da0c0247f907b80fef7ad79b6c280eb7066ce
ichyr <zmii@plast.org.ua>
2015-12-25 23:15:00 +0100

    added security to user#show email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+	def uglify_email_helper(email)
+		temp = inject_some_rand_lyllies(email)
+		temp.join
+	end
+
+	private
+	def inject_some_rand_lyllies temp
+		rand = Random.new
+		temp = temp.split('')
+		count = rand.rand(temp.length)
+		count.times {temp.insert(rand.rand(temp.length), "\u269C")}
+		temp
+	end
 end
