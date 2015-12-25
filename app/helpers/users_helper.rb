271b332ae6cbfee7617d356c1610e2cde69e8bc7
ichyr <zmii@plast.org.ua>
2015-12-25 23:27:07 +0100

    fixed all public places. added spaces near lillies

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,8 +8,8 @@ module UsersHelper
 	def inject_some_rand_lyllies temp
 		rand = Random.new
 		temp = temp.split('')
-		count = rand.rand(temp.length)
-		count.times {temp.insert(rand.rand(temp.length), "\u269C")}
+		count = rand.rand(temp.length / 2)
+		count.times {temp.insert(rand.rand(temp.length), " \u269C ")}
 		temp
 	end
 end
