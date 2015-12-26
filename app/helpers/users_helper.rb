4e23d3b447b29e2bb97e2259c51d8e69cff3d704
ichyr <zmii@plast.org.ua>
2015-12-26 13:46:22 +0100

    minimum one lilly will mask e-mails

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,7 +8,7 @@ module UsersHelper
 	def inject_some_rand_lyllies temp
 		rand = Random.new
 		temp = temp.split('')
-		count = rand.rand(temp.length / 2)
+		count = [1, rand.rand(temp.length / 2)].max
 		count.times {temp.insert(rand.rand(temp.length), " \u269C ")}
 		temp
 	end
