83b1baa9c5c519b1bd53133687975d0837f2a281
ichyr <zmii@plast.org.ua>
2016-05-30 21:44:22 +0200

    added pundit authorization for registries

--- app/controllers/registries_controller.rb
+++ app/controllers/registries_controller.rb
@@ -4,39 +4,47 @@ class RegistriesController < ApplicationController
   respond_to :html
 
   def index
+    authorize :registry, :index?
     @registries = Registry.all
     respond_with(@registries)
   end
 
   def show
+    authorize :registry, :show?
     respond_with(@registry)
   end
 
   def new
+    authorize :registry, :new?
     @registry = Registry.new
     respond_with(@registry)
   end
 
   def edit
+    authorize :registry, :edit?
   end
 
   def create
+    authorize :registry, :create?
     @registry = Registry.new(registry_params)
     @registry.save
     respond_with(@registry)
   end
 
   def update
+    authorize :registry, :update?
     @registry.update(registry_params)
     respond_with(@registry)
   end
 
   def destroy
+    authorize :registry, :destroy?
     @registry.destroy
     respond_with(@registry)
   end
 
   def vmilist_instruktors
+    authorize :registry, :vmilist_instruktors?
     @instruktors = Qualification
     .joins(:user)
     .where("vmilist_id = CAST(? AS INT) and \"users\".\"name\" like ?", params[:vmilist_id], "%#{params[:q]}%")
@@ -49,6 +57,7 @@ class RegistriesController < ApplicationController
   end
 
   def autocomplete_vmilist_name
+    authorize :registry, :autocomplete_vmilist_name?
     params[:q] = params[:q] || ''
     @vmilists = Vmilist.where("name like ?", "%#{params[:q]}%")
     .limit(10)
@@ -59,6 +68,7 @@ class RegistriesController < ApplicationController
   end
 
   def autocomplete_instruktor_name
+    authorize :registry, :autocomplete_instruktor_name?
     params[:q] = params[:q] || ''
     params[:vmilist_id] = params[:vmilist_id] || ''
 
