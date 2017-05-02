97c7355fd6e5c95b79d3e27f5bbaec5d77403ae3
ichyr <zmii@plast.org.ua>
2017-05-02 14:16:04 +0200

    GI-51 altered styling of table

--- app/assets/javascripts/registries_angular/app.js
+++ app/assets/javascripts/registries_angular/app.js
@@ -5,82 +5,80 @@ angular.module('specplast', [
   'ui.grid',
   'ui.grid.grouping'
 ]).controller('RegistryListing', ['$scope',
-    '$http', 'uiGridGroupingConstants', 'i18nService',
-    function($scope, $http, uiGridGroupingConstants, i18nService) {
+  '$http', 'uiGridGroupingConstants', 'i18nService',
+  function ($scope, $http, uiGridGroupingConstants, i18nService) {
 
-      i18nService.setCurrentLang('ua');
+    i18nService.setCurrentLang('ua');
 
-      $scope.gridOptions = {
-        enableFiltering: true,
-        treeRowHeaderAlwaysVisible: false,
-        columnDefs: [
-          { name: 'id', displayName: 'Номер', width: '100', enableGrouping: false },
-          {
-            name: 'name',
-            displayName: 'Ім\'я та Прізвище',
-            width: '200',
-            cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.name}} {{row.entity.surname}}</div>'
-          },
-          {
-            name: 'dob',
-            displayName: 'Рік народження',
-            width: '100',
-            cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.dob}}</div>'
-          },
-          {
-            name: 'sex',
-            displayName: 'Стать',
-            width: '100',
-            cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.sex?"Жін":"Чол"}}</div>'
-          },
-          {
-            name: 'rank',
-            displayName: 'Ступінь',
-            width: '100',
-            cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.rank.title}}</div>'
-          },
-          { name: 'troop', displayName: 'Курінь', width: '100' },
-          { name: 'group', displayName: 'Гурток', width: '100' },
-          { name: 'city', displayName: 'Станиця', width: '100' },
-          { name: 'region', displayName: 'Округа', width: '100' },
-          { name: 'vmilist_id', displayName: 'Вмілість', width: '100' },
-          { name: 'achievement_date', displayName: 'Дата здобуття', width: '100', enableGrouping: false },
-          { name: 'place', displayName: 'Місце здобуття', width: '100' },
-          { name: 'activity', displayName: 'Акція', width: '100' },
-          {
-            name: 'instruktor_id',
-            displayName: 'Інструктор',
-            width: '100',
-            cellTemplate: '<div class="ui-grid-cell-contents"><a href="users/{{row.entity.instruktor_id}}">{{instruktor_id}}</a></div>'
-          },
-          {
-            name: 'comment',
-            displayName: 'Коментар',
-            width: '300',
-            enableGrouping: false
-          },
-          {
-            name: 'photo_url',
-            displayName: 'Посилання на посвідку',
-            width: '200',
-            cellTemplate: '<div class="ui-grid-cell-contents"><a href="{{row.entity.photo_url}}">Перейти</a></div>',
-            enableGrouping: false
-          }
-        ],
-        onRegisterApi: function(gridApi) {
-          $scope.gridApi = gridApi;
+    $scope.gridOptions = {
+      enableFiltering: true,
+      treeRowHeaderAlwaysVisible: false,
+      columnDefs: [
+        { name: 'id', displayName: 'Номер', width: '100', enableGrouping: false },
+        {
+          name: 'name',
+          displayName: 'Ім\'я та Прізвище',
+          width: '200',
+          cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.name}} {{row.entity.surname}}</div>'
+        },
+        {
+          name: 'dob',
+          displayName: 'Рік нар.',
+          width: '100',
+          cellTemplate: '<div class="ui-grid-cell-contents text-center">{{row.entity.dob}}</div>'
+        },
+        {
+          name: 'sex',
+          displayName: 'Стать',
+          width: '100',
+          cellTemplate: '<div class="ui-grid-cell-contents text-center">{{row.entity.sex?"Жін":"Чол"}}</div>'
+        },
+        {
+          name: 'rank',
+          displayName: 'Ступінь',
+          width: '100',
+          cellClass: 'text-center'
+        },
+        { name: 'troop', displayName: 'Курінь', width: '150' },
+        { name: 'group', displayName: 'Гурток', width: '150' },
+        { name: 'city', displayName: 'Станиця', width: '150' },
+        { name: 'region', displayName: 'Округа', width: '150' },
+        { name: 'vmilist', displayName: 'Вмілість', width: '150' },
+        { name: 'achievement_date', displayName: 'Дата здобуття', width: '150', enableGrouping: false },
+        { name: 'place', displayName: 'Місце здобуття', width: '150' },
+        { name: 'activity', displayName: 'Акція', width: '150' },
+        {
+          name: 'instruktor',
+          displayName: 'Інструктор',
+          width: '150',
+          cellTemplate: '<div class="ui-grid-cell-contents txt-center"><a href="users/{{row.entity.instruktor_id}}">{{row.entity.instruktor}}</a></div>'
+        },
+        {
+          name: 'comment',
+          displayName: 'Коментар',
+          width: '300',
+          enableGrouping: false
+        },
+        {
+          name: 'photo_url',
+          displayName: 'Посвідка',
+          width: '100',
+          cellTemplate: '<div class="ui-grid-cell-contents text-center"><a href="{{row.entity.photo_url}}">перейти</a></div>',
+          enableGrouping: false
         }
-      };
+      ],
+      onRegisterApi: function (gridApi) {
+        $scope.gridApi = gridApi;
+      }
+    };
 
 
-      $scope.gridOptions.data = [];
-      $http.get('/registries.json').then(function successCallback(response) {
-        debugger;
-        $scope.gridOptions.data = response.data;
-      }, function errorCallback(response) {
-        debugger;
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-    }
-  ]);
+    $scope.gridOptions.data = [];
+    $http.get('/registries.json').then(function successCallback(response) {
+      $scope.gridOptions.data = response.data;
+    }, function errorCallback(error) {
+      console.log('Error occured. Please refresh web page or contact administrators.');
+      console.log(error);
+    });
+  }
+]);
--- app/views/registries/index.json.jbuilder
+++ app/views/registries/index.json.jbuilder
@@ -1,4 +1,8 @@
 json.array!(@registries) do |registry|
-  json.extract! registry, :id, :name, :surname, :dob, :email, :sex, :rank, :troop, :group, :city, :region, :vmilist_id, :achievement_date, :place, :activity, :instruktor_id, :comment
+  json.extract! registry, :id, :name, :surname, :email, :sex, :rank, :troop, :group, :city, :region, :vmilist_id, :achievement_date, :place, :activity, :instruktor_id, :comment
+  json.dob registry.dob.to_date.year
+  json.rank registry.rank.title
+  json.vmilist registry.vmilist.name
+  json.instruktor registry.instruktor.name
   json.url registry_url(registry, format: :json)
 end
